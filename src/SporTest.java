297093837c92a675642f3b9ede8556258c8f6f1e
miki1989 <mikolajheller@gmail.com>
2018-07-20 13:58:18 +0200

    Updated version but still to change

--- src/SporTest.java
+++ src/SporTest.java
@@ -1,4 +1,3 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class SporTest {
@@ -6,33 +5,45 @@ public class SporTest {
 
         Sport[] sporty = new Sport[3];
         Scanner scanner = new Scanner(System.in);
-        scanner.useLocale(Locale.US);
-
-
-
-
+        int firstEmptyPlace = 0;
 
         System.out.println("Wprowadź informacje dotyczące dyscyplin sportowych");
         for (int i = 0; i < sporty.length; i++) {
-            sporty[i] = new Sport(scanner.nextLine(), scanner.nextLine(), scanner.nextInt());
-            scanner.nextLine();
-            while(i > 0) {
-                if (sporty[i].equals(sporty[(i- 1)])) {
-                    System.out.println("Duplikat, powtórz");
-                    i--;
-                }
-                else if (sporty[i].equals(sporty[(i - i)])) {
-                    System.out.println("Duplikat, powtórz");
-                    i--;
-                }
-                break;
+            Sport tempSport = createSport(scanner);
+            if(!contains(tempSport, sporty)){
+                sporty[firstEmptyPlace] = tempSport;
+                firstEmptyPlace++;
             }
-            System.out.println();
+            else {
+                i--;
+                System.out.println("Taka dyscyplina już istnieje");
+            }
+
         }
         for (int i = 0; i < sporty.length; i++) {
             System.out.println(sporty[i].toString());
         }
+    }
 
-        scanner.close();
+    private static Sport createSport(Scanner scanner){
+        Sport tempSport = new Sport();
+        System.out.println("Podaj nazwę dyscypliny:");
+        tempSport.setName(scanner.nextLine());
+        System.out.println("Podaj kraj pochodzenia");
+        tempSport.setOrigin(scanner.nextLine());
+        System.out.println("Podaj poziom popularności");
+        tempSport.setPopularity(scanner.nextInt());
+        scanner.nextLine();
+        return tempSport;
+    }
+
+    static boolean contains(Sport sport, Sport[] sporty){
+        for(Sport sport1 : sporty){
+            if (sport.equals(sport1)){
+                return true;
+            }
+        }
+        return false;
     }
 }
+
--- src/Sport.java
+++ src/Sport.java
@@ -1,13 +1,9 @@
+import java.util.Objects;
+
 public class Sport {
         private String name;
         private String origin;
         private int popularity;
-
-    public Sport(String name, String origin, int popularity) {
-        this.name = name;
-        this.origin = origin;
-        this.popularity = popularity;
-    }
 
     public String getName() {
         return name;
@@ -38,26 +34,18 @@ public class Sport {
         if(this == o){
             return true;
         }
-        if(o == null){
-            return false;
-        }
-        if(!(o instanceof Sport)){
-            return false;
-        }
+        if(o == null || getClass() != o.getClass()) return false;
         Sport sport = (Sport) o;
-        if (!name.equals(sport.name)){
-            return false;
-        }
-        if (!origin.equals(sport.origin)){
-            return false;
-        }
-        if (popularity != sport.popularity){
-            return false;
-        }
-        return true;
+        return Integer.compare(sport.popularity, popularity) == 0 && Objects.equals(name, sport.name) && Objects.equals(origin, sport.origin);
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(name,popularity,origin);
     }
 
     public String toString(){
         return "Nazwa dyscypliny to "+ name + " kraj pochodzenia to " + origin + " poziom popularnosci " + popularity;
     }
+
+
 }
